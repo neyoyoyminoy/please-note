4db01ff5ae04e2f4022ecb73c236b0f85f590f8f
Brendan Nellis <brendan.e.nellis@gmail.com>
2026-01-26 01:16:14 -0600

    Initial commit: Spring Boot backend skeleton

---
+++ backend/src/main/java/com/pleasenote/PleaseNoteApplication.java
@@ -0,0 +1,13 @@
+package com.pleasenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleaseNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleaseNoteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pleasenote/PleaseNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleasenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleaseNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
