a994f7812e44a4939656400610248a24085e18ee
Brendan Nellis <brendan.e.nellis@gmail.com>
2026-02-01 18:31:50 -0600

    updated file for Notes API

--- backend/src/main/java/com/pleasenote/repository/NoteRevisionRepository.java
+++ backend/src/main/java/com/pleasenote/repository/NoteRevisionRepository.java
@@ -1,10 +1,17 @@
 package com.pleasenote.repository;
 
-import com.pleasenote.model.NoteRevision;
 import com.pleasenote.model.Note;
+import com.pleasenote.model.NoteRevision;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteRevisionRepository extends JpaRepository<NoteRevision, Long> {
+
     List<NoteRevision> findByNoteOrderByRevisionNumber(Note note);
+
+    @Query("SELECT nr FROM NoteRevision nr WHERE nr.note = :note ORDER BY nr.revisionNumber DESC LIMIT 1")
+    Optional<NoteRevision> findLatestByNote(Note note);
 }
